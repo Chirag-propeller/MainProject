d10c0fc25f2a18272954f3184ba1968483f166f3
Chirag Agarwal <chiragagarwal819@gmail.com>
2025-06-24 13:08:58 +0530

    added ZohoCRM.settings.ALL scope

--- src/app/api/integration/crm/zoho/auth/route.ts
+++ src/app/api/integration/crm/zoho/auth/route.ts
@@ -6,8 +6,7 @@ export async function GET() {
   const redirectUri = encodeURIComponent(
     `${baseUrl}api/integration/crm/zoho/callback`
   );
-
-  const authUrl = `https://accounts.zoho.in/oauth/v2/auth?scope=ZohoCRM.modules.ALL&client_id=${clientId}&response_type=code&access_type=offline&redirect_uri=${redirectUri}`;
+  const authUrl = `https://accounts.zoho.in/oauth/v2/auth?scope=ZohoCRM.modules.ALL,ZohoCRM.settings.ALL&client_id=${clientId}&response_type=code&access_type=offline&redirect_uri=${redirectUri}`;
 
   return NextResponse.redirect(authUrl);
 }
